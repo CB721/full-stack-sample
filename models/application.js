37bb308598ac738f6aebb7f756d5ec44cd6d87ed
Clint Brodar <clintbrodar@gmail.com>
2020-04-25 16:53:44 -0700

    Models

--- models/application.js
+++ models/application.js
@@ -1,13 +1,36 @@
 module.exports = function (sequelize, DataTypes) {
   const Application = sequelize.define("Application", {
-    name: DataTypes.STRING
+    applicant_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    admin_id: DataTypes.INTEGER,
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+    approved: { 
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      validate: {
+        len: [10]
+      }
+    }
   });
 
   Application.associate = function (models) {
-    Application.belongsTo(models.Application, {
-      foreignKey: {
-        allowNull: false
-      }
+    Application.belongsTo(models.User, {
+      foreignKey: "applicant_id",
+      as: "applicant",
+      onDelete: "cascade"
+    });
+    Application.belongsTo(models.User, {
+      foreignKey: "admin_id",
+      as: "admin",
+      onDelete: "cascade"
     });
   };
 
--- models/user.js
+++ models/user.js
@@ -1,14 +1,37 @@
-module.exports = function(sequelize, DataTypes) {
-    const User = sequelize.define("User", {
-      name: DataTypes.STRING
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5]
+      }
+    },
+    is_admin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+  User.associate = function (models) {
+    User.hasMany(models.Application, {
+      foreignKey: "applicant_id",
+      as: "applicant",
+      onDelete: "cascade"
     });
-  
-    User.associate = function(models) {
-      User.hasMany(models.Application, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return User;
+    User.hasMany(models.Application, {
+      foreignKey: "admin_id",
+      as: "admin",
+      onDelete: "cascade"
+    });
   };
-  
+
+  return User;
+};
